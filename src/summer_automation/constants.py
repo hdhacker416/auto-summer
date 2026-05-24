cf8e6f32cb97177a636498610d3c0ebdd2cfc9bf
hdhacker416 <128950727+hdhacker416@users.noreply.github.com>
2026-05-24 17:23:20 +0800

    Add Summer UI constants

---
+++ src/summer_automation/constants.py
@@ -0,0 +1,35 @@
+PACKAGE_NAME = "cn.imsummer.summer"
+ADB_KEYBOARD_IME = "com.android.adbkeyboard/.AdbIME"
+
+
+class Ids:
+    TITLE = f"{PACKAGE_NAME}:id/title"
+    TOOLBAR_TITLE = f"{PACKAGE_NAME}:id/toolbar_title"
+
+    MAIN_TAB_LABEL = f"{PACKAGE_NAME}:id/main_tab_laber_tv"
+
+    MESSAGE_LIST = f"{PACKAGE_NAME}:id/main_msg_notification_rv"
+    MESSAGE_NICKNAME = f"{PACKAGE_NAME}:id/msg_item_nickname_tv"
+    MESSAGE_PREVIEW = f"{PACKAGE_NAME}:id/msg_item_msg_tv"
+    MESSAGE_TIME = f"{PACKAGE_NAME}:id/msg_item_time_tv"
+
+    CHAT_INPUT = f"{PACKAGE_NAME}:id/et_sendmessage"
+    CHAT_SEND = f"{PACKAGE_NAME}:id/btn_send"
+    CHAT_MORE = f"{PACKAGE_NAME}:id/btn_more"
+    CHAT_CONTENT = f"{PACKAGE_NAME}:id/tv_chatcontent"
+
+    NEARBY_LIST = f"{PACKAGE_NAME}:id/child_nearby_rv"
+    FRIEND_LIST = f"{PACKAGE_NAME}:id/friend_friend_rv"
+    USER_NICKNAME = f"{PACKAGE_NAME}:id/nearby_user_item_nickname"
+    USER_SCHOOL = f"{PACKAGE_NAME}:id/nearby_user_item_school"
+    USER_AGE = f"{PACKAGE_NAME}:id/nearby_user_item_age"
+    USER_DISTANCE = f"{PACKAGE_NAME}:id/nearby_user_item_distance"
+    USER_BIO = f"{PACKAGE_NAME}:id/nearby_user_item_bio"
+
+    PAPER_LIST = f"{PACKAGE_NAME}:id/other_paper_rv"
+    PAPER_TITLE = f"{PACKAGE_NAME}:id/paper_title_tv"
+    PAPER_SEQUENCE = f"{PACKAGE_NAME}:id/paper_seq_type_tv"
+    PAPER_ESSAY = f"{PACKAGE_NAME}:id/paper_essay_et"
+    PAPER_CHOICE_GROUP = f"{PACKAGE_NAME}:id/paper_choice_rg"
+    PAPER_PUBLIC_SWITCH = f"{PACKAGE_NAME}:id/public_sc"
+    PAPER_CONFIRM = f"{PACKAGE_NAME}:id/other_paper_confirm_tv"
